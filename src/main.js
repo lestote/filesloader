387bc46fcf4fe20416999fbf08d68d947f7168fc
lestote <raphael.gauvain@gmail.com>
2018-04-05 11:16:21 +0200

    code made cleaner

--- src/main.js
+++ src/main.js
@@ -2,19 +2,17 @@ const gifMap = require('./images.map');
 
 const background = () => {
     const randomBackground = `./images/background/${gifMap[0][Math.floor(Math.random() * gifMap[0].length)]}`;
-    setInterval(() => document.getElementById('background').innerHTML = `<img style="opacity: 1" class="cover" src="${randomBackground}"></img>`, 1000);
+    return document.getElementById('background').innerHTML = `<img style="opacity: 1" class="cover" src="${randomBackground}"></img>`;
 };
 
 const cover1 = () => {
     const randomFirstOverlay = `./images/overlayFirst/${gifMap[1][Math.floor(Math.random() * gifMap[1].length)]}`;
-    // setInterval(() => `<img src="${randomFirstOverlay}"></img>`, 500);
-    setInterval(() => document.getElementById('cover1').innerHTML = `<img style="opacity: 0.25" class="cover" src="${randomFirstOverlay}"></img>`, 200);
+    return document.getElementById('cover1').innerHTML = `<img style="opacity: 0.25" class="cover" src="${randomFirstOverlay}"></img>`;
 };
 
 const cover2 = () => {
     const randomSecondOverlay = `./images/overlaySecond/${gifMap[2][Math.floor(Math.random() * gifMap[2].length)]}`;
-    // setInterval(() => `<img src="${randomSecondOverlay}"></img>`, 250);
-    setInterval(() => document.getElementById('cover2').innerHTML = `<img style="opacity: 0.25" class="cover" src="${randomSecondOverlay}"></img>`, 500);
+    return document.getElementById('cover2').innerHTML = `<img style="opacity: 0.25" class="cover" src="${randomSecondOverlay}"></img>`;
 };
 
 
